b3f06dbd813392ec91cf690f08b33c571bc11d4e
Joe <joe.lonsdale@eg-otm.com>
2025-12-02 15:58:37 +0000

    add comments

--- day2/main.py
+++ day2/main.py
@@ -12,8 +12,11 @@ def main():
         while pointer <= int(b):
             pointer_str = str(pointer)
             for x in range(1, len(pointer_str)):
+                # check if we can get chunks of equal length x
                 if len(pointer_str) % x == 0:
+                    # if so, calculate how many chunks we need
                     chunks = int(len(pointer_str)/x)
+                    # if original string == first chunk repeated N times, then the original string is an invalid id
                     if(pointer_str == [pointer_str[:x]*chunks][0] and pointer not in invalid_ids):
                         print(f"{pointer_str}")
                         invalid_ids.append(pointer)
