4bb76b8540926f4be99aa777df13c490da4c1707
Joe <joe.lonsdale@eg-otm.com>
2025-12-03 11:10:16 +0000

    remove redundant else

--- day3/main_min_lines.py
+++ day3/main_min_lines.py
@@ -1,4 +1,4 @@
 def f(n, b):
-    return sorted(b)[-1] if n == 1 else b if n == len(b) else f'{b[b.find(max(b[:len(b) - n + 1]))]}{f(n-1, b[b.find(max(b[:len(b) - n + 1]))+1:])}'
+    return sorted(b)[-1] if n == 1 else f'{b[b.find(max(b[:len(b) - n + 1]))]}{f(n-1, b[b.find(max(b[:len(b) - n + 1]))+1:])}'
 i = [l.replace("\n","") for l in open("input.txt", "r").readlines()]
 print(f"part 1: {sum([int(f(2,l)) for l in i])}\npart 2: {sum([int(f(12,l)) for l in i])}")
