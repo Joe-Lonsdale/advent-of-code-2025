f853df9b8f40991e55fe68f87c1135dc80e9f4fd
Joe <joe.lonsdale@eg-otm.com>
2025-12-05 15:52:05 +0000

    day5 pt1 + 2

--- day5/main.py
+++ day5/main.py
@@ -5,7 +5,37 @@ filename = "input.txt"
 file = open(filename, "r")
 
 def main():
-    input = [l.split("-") for l in file.readline().split(",")]
+    fresh = []
+    available = []
+    for l in [s.replace("\n","") for s in file.readlines()]:
+        if "-" in l:
+            fresh.append([int(s) for s in l.split("-")])
+        elif len(l) != 0:
+            available.append(int(l))
 
+    # part 1
+    fresh_count = 0
+    for a in available:
+        for f in fresh:
+            if a in range(f[0], f[1]+1):
+                fresh_count += 1
+                break
+    print(f"part 1: {fresh_count}")
+
+    # part 2
+    sorted_ranges = sorted(fresh,key=lambda x: x[0])
+    changed = True
+    while changed:
+        changed = False
+        for i,r in enumerate(sorted_ranges[:-1]):
+            if sorted_ranges[i][1] >= sorted_ranges[i+1][0] - 1:
+                sorted_ranges[i][1] = max(sorted_ranges[i][1], sorted_ranges[i+1][1])
+                sorted_ranges.remove(sorted_ranges[i+1])
+                changed=True
+                break
+    print(sum([r[1] - r[0] + 1 for r in sorted_ranges]))
+
+
+    
 if __name__ == "__main__":
     main()
